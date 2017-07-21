0f65be5f38a1437fe0c9de12815017b125dfe234
lwq945 <lwq945@icloud.com>
2017-07-21 21:35:33 +0800

    webpack-demo

--- webpack-demo/dist/js/bundle.js
+++ webpack-demo/dist/js/bundle.js
@@ -403,7 +403,7 @@ waterfallLoad.prototype={
 		this.callback=callback;
 		var _this=this
 		$.ajax({
-	    url: 'http://platform.sina.com.cn/slide/album_tech',
+	    url: '//platform.sina.com.cn/slide/album_tech',
 	    dataType: 'jsonp',
 	    jsonp: "jsoncallback",
 	    data:{
